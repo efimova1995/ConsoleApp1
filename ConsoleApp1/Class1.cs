628059714dc89401d9b7886a890498dc5bef40d3
efimova <efimovali55@icloud.com>
2024-04-18 12:21:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers(5, 10);
+        }
+
+        static void PrintNumbers(int m, int n)
+        {
+            if (m > n)
+            {
+                return;
+            }
+            Console.WriteLine(m);
+            PrintNumbers(m + 1, n);
+        }
+    }
+}
---
+++ ConsoleApp1/Class3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class3
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            PrintArrayFromEnd(array, array.Length - 1);
+        }
+
+        static void PrintArrayFromEnd(int[] array, int index)
+        {
+            if (index < 0)
+            {
+                return;
+            }
+            Console.WriteLine(array[index]);
+            PrintArrayFromEnd(array, index - 1);
+        }
+    }
+}
